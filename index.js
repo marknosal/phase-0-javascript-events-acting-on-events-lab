669327161c0c3f1814ec9d5315b383a31378e383
Mark Nosal <mark.c.nosal@gmail.com>
2023-01-05 23:38:24 -0600

    Finished a fun lab!

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left -5}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace('px', "");
+    const right = parseInt(rightNumbers, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 5}px`;
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    } else if (event.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+});
